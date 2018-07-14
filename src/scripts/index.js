f75700adb5a32fdbf2eae27075a88e2d86a5b0d5
nickells <ellsworth.nick@gmail.com>
2018-07-14 15:30:20 -0400

    made touch compatible

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 /*
  todo:
  touch capable
+ tap tempo
  emphasis chooser
  sound pick
  type or drag
@@ -35,31 +36,46 @@ function tick(){
 }
 tick()
 
-const increase = () => {
-  bpm++
-  interval = bpmToInterval(bpm)
-  $bpm.innerHTML = bpm
-}
-const decrease = () => {
-  bpm--
-  interval = bpmToInterval(bpm)
-  $bpm.innerHTML = bpm
+
+// keep track of values for "wheel" functionality
+let lastTick = 0
+let startBpm = bpm
+
+const getEventFromTouchOrMouse = (event) => event.touches ? event.touches[event.touches.length - 1] : event
+
+const normalizeEventListener = (func) => {
+  return function eventHandler(event) {
+    return func(getEventFromTouchOrMouse(event))
+  }
 }
 
-document.body.addEventListener('mousedown', (event) => {
+const downListener = normalizeEventListener((event) => {
   isMouseDown = true
   originalPosition = [event.clientX, event.clientY]
 })
 
-document.body.addEventListener('mouseup', (event) => {
+const upListener = normalizeEventListener((event) => {
   isMouseDown = false
+  startBpm = bpm
 })
 
-document.body.addEventListener('mousemove', (event) => {
+const moveListener = normalizeEventListener((event) => {
   if (!isMouseDown) return
   const [originalX, originalY] = originalPosition
-  if (event.clientX > originalX) {
-    increase()
+  const diff = event.clientX - originalX
+  // Only perform an operation if you have moved 10 pixels
+  const ticks = Math.floor(diff / 10)
+  if (ticks !== lastTick) {
+    bpm = startBpm + (ticks * 2)
+    interval = bpmToInterval(bpm)
+    $bpm.innerHTML = bpm
+    lastTick = ticks
   }
-  else decrease()
 })
+
+document.body.addEventListener('touchstart', downListener)
+document.body.addEventListener('mousedown', downListener)
+document.body.addEventListener('touchend', upListener)
+document.body.addEventListener('mouseup', upListener)
+document.body.addEventListener('touchmove', moveListener)
+document.body.addEventListener('mousemove', moveListener)
